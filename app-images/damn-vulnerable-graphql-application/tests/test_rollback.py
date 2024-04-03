aa7f4926303e566ccb1d6eacfa7f3d64401ebd5f
mfoster_stackrox <michael.foster@stackrox.com>
2024-04-03 14:58:51 -0400

    updated

--- app-images/damn-vulnerable-graphql-application/tests/test_rollback.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-from common import URL
-
-def test_check_rollback():
-    r = requests.get(URL + '/start_over')
-    assert r.status_code == 200
-    assert 'Restored to default state' in r.text
