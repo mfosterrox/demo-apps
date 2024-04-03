6d70c3780b449a9ce13453a90c799ca30e3bdaa7
mfoster_stackrox <michael.foster@stackrox.com>
2024-04-03 13:54:13 -0400

    updating labels

--- app-images/central-api-manipulator/README.md
+++
@@ -1,5 +0,0 @@
-# RHACS Central manipulator
-
-## uses the ACS API to set up Central for demo purposes
-
-
--- app-images/central-api-manipulator/build390.sh
+++
@@ -1,2 +0,0 @@
-./mvnw package
-podman build --platform linux/s390x -t quay.io/rhacs-misc/centralapi:os390 ./ -f ./Dockerfile.os390
